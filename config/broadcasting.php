6ec766c71bc25e4e3b48b5d30d6e7e0a15f807f2
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-05-06 02:02:20 +0700

    fix: update Pusher host and port configuration for broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -51,14 +51,8 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
-                'scheme' => env('PUSHER_SCHEME', 'http'),
-                'useTLS' => env('PUSHER_SCHEME') === 'https',
-            ],
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'use_tls' => true,
         ],
 
         'ably' => [
